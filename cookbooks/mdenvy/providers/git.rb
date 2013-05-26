135deb12f515bc4b628e7cd25cb5f5409254822e
Gereon Frey <me@gereonfrey.de>
2013-05-26 14:55:43 +0200

    added chef LWRP for cloning private git repositories
    Cloning private repositories requires access to ssh keys that are forwarded for
    the root user, but not the newly created user. This is why cloning is initially
    done using root.

---
+++ cookbooks/mdenvy/providers/git.rb
@@ -0,0 +1,44 @@
+
+def whyrun_supported?
+  true
+end
+
+action :create do
+  if @current_resource.exists
+    Chef::Log.info "#{@new_resource} already exists - nothing to do."
+  else
+    converge_by("Create #{@new_resource}") do
+      checkout_repo
+    end
+  end
+end
+
+def load_current_resource
+  @current_resource = Chef::Resource::MdenvyGit.new(@new_resource.repository)
+  @current_resource.name(@new_resource.name)
+  @current_resource.repository(@new_resource.repository)
+  @current_resource.path(@new_resource.path)
+  @current_resource.user(@new_resource.user)
+  @current_resource.exists = ::File.directory?("/home/#{@new_resource.user}/development/{@new_resource.path}")
+end
+
+private
+
+def checkout_repo
+  user_home = "/home/#{new_resource.user}"
+  repo_path = "#{user_home}/development/#{new_resource.path}"
+
+  directory repo_path do
+    mode "0755"
+    action :create
+  end
+
+  git repo_path do
+    repository new_resource.repository
+    action :sync
+  end
+
+  execute "change ownership" do
+    command "chown -R #{new_resource.user}:users #{repo_path}"
+  end
+end
---
+++ cookbooks/mdenvy/resources/git.rb
@@ -0,0 +1,10 @@
+
+actions :create
+
+default_action :create
+
+attribute :repository, :kind_of => String, :name_attribute => true, :required => true
+attribute :user,       :kind_of => String, :required => true
+attribute :path,       :kind_of => String
+
+attr_accessor :exists
