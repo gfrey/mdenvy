639582673610e4f6bedf5f4925617502807b09b8
Gereon Frey <me@gereonfrey.de>
2013-06-28 10:20:49 +0200

    improved my personal stuff
    - use username provided by vagrant to be more flexible with different username
      schemes (gfrey vs. gerfre).
    - fixed bug with git (make root directory world accessible) that should be gone
      with git 1.8.3 version.

--- cookbooks/gfrey/recipes/default.rb
+++ cookbooks/gfrey/recipes/default.rb
@@ -1,9 +1,9 @@
-user_home = "/home/gfrey"
+user_home = "/home/#{node[:mdenvy][:user]}"
 
 ## -----------------------------------------------------------------------------
 ## Create directory with all user related configuration.
 directory "#{user_home}/usr/etc" do
-  owner "gfrey"
+  owner node[:mdenvy][:user]
   group "users"
   mode "0755"
   recursive true
@@ -11,10 +11,15 @@ directory "#{user_home}/usr/etc" do
 end
 
 directory "#{user_home}/usr/bin" do
-  owner "gfrey"
+  owner node[:mdenvy][:user]
   group "users"
   mode "0755"
   action :create
+end
+
+## Fix bug with with git 1.8.1.2 (fixed in 1.8.3)
+directory '/root' do
+  mode 0755
 end
 
 ## -----------------------------------------------------------------------------
@@ -24,18 +29,18 @@ package 'zsh-beta'
 git "#{user_home}/usr/etc/oh-my-zsh" do
   repository "https://github.com/gfrey/oh-my-zsh.git"
   revision "personal"
-  user "gfrey"
+  user node[:mdenvy][:user]
   group "users"
   action :sync
 end
 
 link "#{user_home}/.zshrc" do
   to "#{user_home}/usr/etc/oh-my-zsh/custom/zshrc"
-  owner "gfrey"
+  owner node[:mdenvy][:user]
   group "users"
 end
 
-user 'gfrey' do
+user node[:mdenvy][:user] do
   shell '/usr/bin/zsh'
   action :modify
 end
@@ -51,14 +56,14 @@ package 'aspell-de'
 git "#{user_home}/usr/etc/prelude" do
   repository "https://github.com/gfrey/prelude.git"
   revision "personal"
-  user "gfrey"
+  user node[:mdenvy][:user]
   group "users"
   action :sync
 end
 
 link "#{user_home}/.emacs.d" do
   to "#{user_home}/usr/etc/prelude"
-  owner "gfrey"
+  owner node[:mdenvy][:user]
   group "users"
 end
 
@@ -72,33 +77,33 @@ package 'python-psutil'
 git "#{user_home}/usr/etc/tmuxified" do
   repository "https://github.com/gfrey/tmuxified.git"
   revision "personal"
-  user "gfrey"
+  user node[:mdenvy][:user]
   group "users"
   action :sync
 end
 
 link "#{user_home}/.tmux" do
   to "#{user_home}/usr/etc/tmuxified"
-  owner "gfrey"
+  owner node[:mdenvy][:user]
   group "users"
 end
 
 link "#{user_home}/.tmux.conf" do
   to "#{user_home}/.tmux/tmux.conf"
-  owner "gfrey"
+  owner node[:mdenvy][:user]
   group "users"
 end
 
 link "#{user_home}/usr/bin/basic-cpu-and-memory.tmux" do
   to "#{user_home}/.tmux/scripts/basic-cpu-and-memory.tmux"
-  owner "gfrey"
+  owner node[:mdenvy][:user]
   group "users"
 end
 
 
 ## -----------------------------------------------------------------------------
 ## Configure git.
-mdenvy_gitconfig "gfrey" do
+mdenvy_gitconfig node[:mdenvy][:user] do
   username "Gereon Frey"
   email "me@gereonfrey.de"
 end
